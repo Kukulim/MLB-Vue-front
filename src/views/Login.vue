6c1f19fb0fc61011a95e187a04c60bee01895769
Kukulim <beckon@interia.pl>
2020-10-27 15:55:40 +0100

    + register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>
-login
-    </div>
+<h1 class="m-5">
+  Login page:
+</h1>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">User Name</label>
+    <input type="text" class="form-control" placeholder="name..." v-model="currentUser.username">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" placeholder="password..." v-model="currentUser.password">
+  </div>
+
+  <button type="submit" class="btn btn-primary" @click="Login()">Login</button>
+</form>
 </template>
 
 <script>
-    export default {
-        
+  import * as axios from 'axios';
+
+  export default {
+      data() {
+    return {  
+      currentUser:{
+        username:"",
+        password:""
+      },
+    };
+  },
+  methods:{
+    Login(){
+        try{
+console.log("login");
+  } catch (error) {
+    console.error(error);
+    return null;
+}
     }
+  },
+  }
 </script>
 
 <style lang="scss" scoped>
