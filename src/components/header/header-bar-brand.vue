6b95b8d5f3b0ee819daf65733ce981f3282037ba
Kukulim <beckon@interia.pl>
2020-11-10 14:20:26 +0100

    bootstrap themes

--- src/components/header/header-bar-brand.vue
+++ src/components/header/header-bar-brand.vue
@@ -4,7 +4,14 @@
         to="/"
         class="nav-link "
       >
-        <span class="navbar-brand"><span>B</span>ooks</span>
+        <span class="navbar-brand"><span class="logo">MLB</span>My Little Bookshelf</span>
       </router-link>
   </div>
 </template>
+<style lang="scss" scoped>
+ .logo{
+   color: cadetblue;
+   font-size: 1.3em;
+   margin: 5px;
+ }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<a href='https://pl.freepik.com/wektory/winobranie'>Winobranie plik wektorowy utworzone przez upklyak - pl.freepik.com</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
